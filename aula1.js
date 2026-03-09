f962dc6722a414382c27ea8cc8f8fbda8a6b3010
aamandayk <amanda.freitas15@escola.pr.gov.br>
2026-03-09 08:57:02 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,14 @@
+// ctrl + s (salvar)
+// const - variável constante ( não pode ser alterada)
+// let - variável local
+// var - variável global 
+console.log("Olá Mundo")
+
+let nome = "Amanda"
+let altura = 1.51
+let idade = 15
+let sexo = "menina"
+let cabelo = "castanhos"
+let pele = "morena."
+console.log("Meu nome é " + nome + " tenho " + idade + "anos de idade! " + "E " + altura + " de altura. Sou uma "+ sexo+ " de cabelos " + cabelo + " e pele " pele )
+
