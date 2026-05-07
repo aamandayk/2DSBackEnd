b0cd2c8d492f3078ac74a5d4840ac84e4f534063
aamandayk <amanda.freitas15@escola.pr.gov.br>
2026-05-07 08:57:05 -0300

    Add files via upload

---
+++ sistema.js
@@ -0,0 +1,55 @@
+
+const fs = require("fs");
+let produtos = require("./dados");
+
+
+
+function mostrar() {
+  console.log(produtos);
+}
+
+
+
+function adicionar() {
+  let novo = {
+    nome: "Chocolate",
+    preco: 7,
+    marca: "KKKK",
+    estoque: 20,
+    codigo: "2526"
+  };
+
+  produtos.push(novo);
+  fs.writeFileSync("produtos.json", JSON.stringify(produtos));
+}
+
+
+
+function filtrar() {
+  let baratos = produtos.filter(p => p.preco < 10);
+  console.log(baratos);
+}
+
+
+
+function desconto() {
+  produtos.forEach(p => {
+    p.preco = p.preco * 0.9;
+  });
+
+  fs.writeFileSync("produtos.json", JSON.stringify(produtos));
+}
+
+
+
+function buscar() {
+  let achado = produtos.find(p => p.nome === "Arroz");
+  console.log(achado);
+}
+
+
+mostrar();
+adicionar();
+filtrar();
+desconto();
+buscar();
